93a2fe16eeac4eec077d1870adc60c12233fc4e1
gitthathonor <jeong.suyoung92@gmail.com>
2022-07-16 14:45:21 +0900

    스프링 웹 개발 기초 - 정적 컨텐츠, MVC와 템플릿 엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,6 +13,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","spring");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name",re) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
 
